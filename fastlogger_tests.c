60c0260f48bfb1c96b0aa0ae8844ae5c4abc7551
Anthony C <agcarat@gmail.com>
2013-11-25 11:43:23 -0500

    made tests in to suite_tests with common destroy function

--- fastlogger_tests.c
+++ fastlogger_tests.c
@@ -5,16 +5,19 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+SUITE_DESTROY(FL) {
+	fastlogger_close();
+	return 0;
+}
 
-TEST(logg_global_debug_true) {
+SUITE_TEST(FL,logg_global_debug_true) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_DEBUG);
 	Log(FL_KEY_INFO, "test log %d\n", i++);
 	AssertEqInt(i, 1);
-	fastlogger_close();
 	return 0;
 }
-TEST(logg_global_threaded_debug_true) {
+SUITE_TEST(FL,logg_global_threaded_debug_true) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_DEBUG);
 	fastlogger_separate_log_per_thread(1) ;
@@ -32,7 +35,7 @@ void * thread_func(void *ptr) {
 	return NULL;
 }
 
-TEST(logg_global_threaded_debug_true_two_threads) {
+SUITE_TEST(FL,logg_global_threaded_debug_true_two_threads) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_DEBUG);
 	fastlogger_separate_log_per_thread(1) ;
@@ -46,7 +49,7 @@ TEST(logg_global_threaded_debug_true_two_threads) {
 	fastlogger_close();
 	return 0;
 }
-TEST(logg_global_threaded_debug_true_secondtime) {
+SUITE_TEST(FL,logg_global_threaded_debug_true_secondtime) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_DEBUG);
 	fastlogger_separate_log_per_thread(1) ;
@@ -60,13 +63,13 @@ TEST(logg_global_threaded_debug_true_secondtime) {
 }
 char * fastlogger_thread_local_file_name(const char * name, int i);
 char * fastlogger_thread_local_file_name(const char * name, int i);
-TEST(logg_thread_file_name) {
+SUITE_TEST(FL,logg_thread_file_name) {
 	char * str =  fastlogger_thread_local_file_name("output.log",0);
 	AssertEqStr(str, "output_t0.log");
 	free(str);
 	return 0;
 }
-TEST(logg_global_debug_false) {
+SUITE_TEST(FL,logg_global_debug_false) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_ERROR);
 	Log(FL_KEY_INFO, "test log %d\n", i++);
@@ -76,7 +79,7 @@ TEST(logg_global_debug_false) {
 
 FastLoggerNS_t testns_1 = FASTLOGGERNS_INIT(namespace_root);
 
-TEST(logg_ns_debug_false) {
+SUITE_TEST(FL,logg_ns_debug_false) {
 	int i=0;
 	//fastlogger_set_loglevel("FL_ERROR,namespace_root,FL_ERROR");
 	fastlogger_set_min_default_log_level(FL_DEBUG);
@@ -86,21 +89,21 @@ TEST(logg_ns_debug_false) {
 	AssertEqInt(i, 0);
 	return 0;
 }
-TEST(logg_ns_global_debug_false) {
+SUITE_TEST(FL,logg_ns_global_debug_false) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_ERROR);
 	LogNS(testns_1,FL_KEY_INFO, "test log %d\n", i++);
 	AssertEqInt(i, 0);
 	return 0;
 }
-TEST(logg_ns_debug_true) {
+SUITE_TEST(FL,logg_ns_debug_true) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_KEY_INFO);
 	LogNS(testns_1, FL_KEY_INFO, "test log %d\n", i++);
 	AssertEqInt(i, 1);
 	return 0;
 }
-TEST(logg_ns_global_debug_true) {
+SUITE_TEST(FL,logg_ns_global_debug_true) {
 	int i=0;
 	fastlogger_set_min_default_log_level(FL_DEBUG);
 	LogNS(testns_1,FL_DEBUG, "test log %d\n", i++);
