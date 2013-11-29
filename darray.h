f5eb7f54b7506f34a503ec099a39db7cb506c008
Anthony C <agcarat@gmail.com>
2013-11-28 21:45:29 -0500

    added dynaArray destructor

--- darray.h
+++ darray.h
@@ -1,5 +1,6 @@
 #ifndef DARRAY_H
 #define DARRAY_H
+#include <string.h> // memset
 typedef void * DynaArray ;
 
 typedef struct _DynaArrayHead_t {
@@ -11,11 +12,19 @@ static inline DynaArray da_create(size_t reserve, void free_func(void *)) {
 	DynaArrayHead_t *headp = malloc (sizeof(DynaArrayHead_t) + sizeof(void *) * reserve );
 	headp->reserve = reserve;
 	headp->free_func = free_func;
-	memset (headp + sizeof(DynaArrayHead_t), 0, sizeof(void *)*reserve);
-	return headp + sizeof(DynaArrayHead_t);
+	memset (headp+1, 0, sizeof(void *)*reserve);
+	return headp +1;
 }
 static inline void da_destroy(DynaArray *ap) {
-
+	DynaArrayHead_t *headp = (DynaArrayHead_t *)ap;
+	headp = headp-1;
+	int i=0;
+	while (ap[i]!=NULL) {
+		headp->free_func(ap[i]);
+		ap[i] = NULL;
+		i++;
+	}
+	free(headp);
 }
 
 #endif
